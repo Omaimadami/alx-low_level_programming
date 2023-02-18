953fa1c23738b1db42a544cf47c3cda048cfd59c
Omaimadami <damiomaima@gmail.com>
2023-02-17 18:59:38 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int x;
+	
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	for (x = 0, x < 26; x++)
+	{
+		putchar(ALP[x]);
+	}
+	putchar('\n');
+	return (0);
+}
