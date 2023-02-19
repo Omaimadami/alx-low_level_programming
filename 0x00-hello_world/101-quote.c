53ea5791fc33e9f79a70f5e40fc4ab910498b732
Omaimadami <damiomaima@gmail.com>
2023-02-19 09:01:37 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define print pritf("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n)
+/**
+ * main - Entry point
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	if (print)
+	{ }
+	return (1);
+}
