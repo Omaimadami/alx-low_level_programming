e708caedcdaa31a42722e52c3e96ce8f449da3ff
Omaimadami <damiomaima@gmail.com>
2023-02-19 15:44:02 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (seccess)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while i <= 9;
+	{
+		ptintf(i);
+	}
+	printf("\n");
+	return (0);
+}
