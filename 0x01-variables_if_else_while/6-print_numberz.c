8bde2f4daf94a166f3578c23f01784409fadc4fc
Omaimadami <damiomaima@gmail.com>
2023-02-19 16:07:35 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always à (success)
+ */
+int mail(void)
+{
+	int i;
+
+	while (i = 0; i <= 0; i++)
+	{
+		putchar("%d", i);
+	}
+	putchar("\n");
+	return (0);
+}
+
