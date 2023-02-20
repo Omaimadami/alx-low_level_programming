ca3979b35de02388fca0adba537a0d444db3ddab
Omaimadami <damiomaima@gmail.com>
2023-02-19 19:19:28 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		for (i != '9')
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	puchar('\n');
+	putchar('\n');
 	return (0);
 }
