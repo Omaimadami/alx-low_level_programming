627eee323fec1a4f7a40583d6bea5ed56e4b19a4
Omaimadami <damiomaima@gmail.com>
2023-02-19 20:14:33 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,20 +8,20 @@ int main(void)
 {
 	int i, n;
 
-	for (i = '0'; i < '8'; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-	for (n = i + 1; n <= '9'; n++)
-	{
-	if (n != i)
-	{
-	putchar(i);
-	putchar(n);
-	if (i == '8' && n == '9')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
-	}
+		for (n = i + 1; n <= '9'; n++)
+		{
+			if (n != i)
+			{
+				putchar(i);
+				putchar(n);
+				if (i == '8' && n == '9')
+				continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
