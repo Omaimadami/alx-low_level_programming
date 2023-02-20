3768a2d840635427647f05505bcc08fd183363d6
Omaimadami <damiomaima@gmail.com>
2023-02-19 18:30:41 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	while (i = "z"; i <= "z"; i--)
+	{
+		putchar("i");
+	}
+	putchar("\n");
+	return (0);
+}
