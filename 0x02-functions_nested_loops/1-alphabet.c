d22789517fcf51bb7280b956a3e7a20d18734015
Omaimadami <damiomaima@gmail.com>
2023-02-21 17:08:19 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
