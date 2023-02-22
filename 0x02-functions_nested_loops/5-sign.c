da2de301c5eea0f329e0d7a78d6a513f08c27528
Omaimadami <damiomaima@gmail.com>
2023-02-21 18:57:25 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * print_sign - Prints the sign of a number
+ * @n: number to check
+ * Return: 1 (n greater than zero), 0 (n is zero), or -1 (n less than zero)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
