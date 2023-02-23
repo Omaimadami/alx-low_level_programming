2f9b34f68c5be54047e9029fc165ee828df8ac98
Omaimadami <damiomaima@gmail.com>
2023-02-22 18:34:52 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 9
+ * @n : number to start from
+ * return: numbers
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			}
+				printf("%d, ", n);
+			}
+		}
+	}
+}
