d49a737f97c001f1e47e8aa2885c566ece29f0e8
Omaimadami <damiomaima@gmail.com>
2023-02-23 17:30:23 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: no return
+ */
+void more_numbers(void);
+{
+	int i, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			_putchar(n + '0');
+		}
+		_putchar('\n');
+	}
+}
