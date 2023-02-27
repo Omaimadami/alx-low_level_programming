d4a7a57f8668607091ed726a7fc8947bf24d63b5
Omaimadami <damiomaima@gmail.com>
2023-02-27 15:17:38 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	for (s = 0; s != '\0'; s++)
+		_putchar(s);
+}
