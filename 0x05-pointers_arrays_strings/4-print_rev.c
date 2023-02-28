7a713d889fc96e7d3bc2a19fee2d9938287d860d
Omaimadami <damiomaima@gmail.com>
2023-02-27 16:02:37 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - that prints a string, in reverse, followed by a new line
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	for (s < '\0'; s >= 0; s--)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
