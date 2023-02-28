85bdd7e679d6f507b08717b03c10a34e8ae28e5c
Omaimadami <damiomaima@gmail.com>
2023-02-27 16:48:14 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int c = 0, i;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rev = s[i];
+		s[i] = s[c];
+		s[c] = rev;
+	}
+}
