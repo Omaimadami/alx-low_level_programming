f3e787e52ea03538b54a729e86ab0747ca98711a
Omaimadami <damiomaima@gmail.com>
2023-03-02 18:01:16 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[x] != '\0' && s2[x] != '\0')
 	{
-		return (s1[x] - s2[x]);
-		x++;
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+	x++;
 	}
+	return (0);
 }
