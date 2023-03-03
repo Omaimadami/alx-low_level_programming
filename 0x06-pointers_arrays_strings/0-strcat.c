13c16d7554d08aed9311b0685424042c7bb689c9
Omaimadami <damiomaima@gmail.com>
2023-03-02 16:22:54 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcat - unction that concatenates two strings
+ * @*dest: string destination
+ * @*src: string source
+ * Return: a pointer to a string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x = 0, y;
+
+	while (dest[x])
+	{
+		x++;
+	}
+	for (y = 0; src[y] != 0; y++)
+	{
+		dest[x] = src[y];
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
