f7cbda572af1cc79256f44c7055f45ba73d676d4
Omaimadami <damiomaima@gmail.com>
2023-03-06 17:31:02 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,11 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
 	{
-		if (*s == c)
+		if (s[i] == c)
 		return (*s);
-		s++;
 	}
 	return (NULL);
 }
