fd705c8c2d353bb81ca0a65b7da54aadd5d48e73
Omaimadami <damiomaima@gmail.com>
2023-03-07 16:54:23 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,9 +8,9 @@ void print_chessboard(char (*a)[8])
 {
 	int r, c;
 
-	for (r = 0, r < 0, r++)
+	for (r = 0, r < 8, r++)
 	{
-		for (c = 0, c < 0, c++)
+		for (c = 0, c < 8, c++)
 		{
 			_putchar(a[r][c]);
 		}
