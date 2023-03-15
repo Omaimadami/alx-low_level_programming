9466104f28be81fc53f0033a8a6bfbb0e2c159f8
Omaimadami <damiomaima@gmail.com>
2023-03-15 08:15:58 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,12 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	(void) argv;
 
-	printf("%d\n", argc);
-	for (i = 0; i < argc - 1; i++)
-	{
-		printf("%s", *argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
