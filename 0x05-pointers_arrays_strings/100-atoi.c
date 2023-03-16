c35d85ab6321b28997436e4008286415d1c16f6a
Omaimadami <damiomaima@gmail.com>
2023-03-15 18:34:11 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,4 +6,16 @@
  */
 int _atoi(char *s)
 {
+	int i = 0, sign = 1, num = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	for (; s[i] != '\0'; ++i)
+	{
+		num = (num * 10) + (s[i] - '0');
+	}
+	return (sign * num);
 }
