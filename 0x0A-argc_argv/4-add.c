3a05fcdc1c4b444e9cb71136c78b3cd289888431
Omaimadami <damiomaima@gmail.com>
2023-03-29 21:08:20 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 /**
  * main - program that adds positive numbers
  * @argc: number of arguments
@@ -8,21 +10,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int add = 0, i;
+	int add = 0, i, j;
 
 	if (argc > 1)
 	{
-		for (i = 0, i < argc, i++)
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[i] < 48 || argv[i] > 57)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				add += atoi(argv[i]);
-			}
+			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
 	}
