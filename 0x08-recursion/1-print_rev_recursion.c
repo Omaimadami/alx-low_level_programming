c906aa0ef37866ed5c25346ef6118328baa39754
Omaimadami <damiomaima@gmail.com>
2023-06-07 18:38:47 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,13 +4,14 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed in reverse.
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-    {
-	    return;
-    }
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
 
