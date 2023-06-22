39b87b1011fe8b1e0cc8bd1148a2d547873bb38d
Omaimadami <damiomaima@gmail.com>
2023-06-21 19:51:09 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 		return (99);
 	}
 
-	if ((!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		return (100);
