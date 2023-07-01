5e9de83c1f30c91256b0f51f97b9362f32f6a85b
Omaimadami <damiomaima@gmail.com>
2023-07-01 15:51:58 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+    format db "Hello, Holberton", 0
+
+section .text
+    extern printf
+
+global main
+main:
+    sub rsp, 8
+    mov edi, format
+    xor eax, eax
+    call printf
+    add rsp, 8
+
+    mov eax, 0
+    ret
