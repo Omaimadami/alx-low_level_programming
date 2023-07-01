dca2687dab7dc64ce82b460c1936bea9ada9b999
Omaimadami <damiomaima@gmail.com>
2023-07-01 16:09:12 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a linked list.
- * @h: Pointer to the head of the linked list.
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes in the linked list.
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
