cfd618e764c0785d4648874b7ec093a2cef8980d
Omaimadami <damiomaima@gmail.com>
2023-07-01 15:35:11 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * print_before_main - Prints a specific message before the main function is executed.
+ * print_before_main - Prints message before the main function is executed.
  */
 void print_before_main(void) __attribute__((constructor));
 
-/**
- * print_before_main - Prints a specific message before the main function is executed.
- */
 void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
