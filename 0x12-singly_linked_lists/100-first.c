816d6e6172d5a3f83fe9ab96890ab7bd26514040
Omaimadami <damiomaima@gmail.com>
2023-07-01 15:32:32 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,11 @@
  */
 void print_before_main(void) __attribute__((constructor));
 
+/**
+ * print_before_main - Prints a specific message before the main function is executed.
+ */
 void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
