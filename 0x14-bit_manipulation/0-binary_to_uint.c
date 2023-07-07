7c78f82ad4a1711f4d9bcedfa7d6055d9f1a505a
Omaimadami <damiomaima@gmail.com>
2023-07-06 17:16:26 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	while (*b != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
 
 		num <<= 1;
-		num += b[i] - '0';
+		num += (*b - '0');
+		b++;
 	}
 
 	return (num);
